b984199b42f6ce4b03cd0448a50aa33e66159497
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2022-11-15 16:00:34 -0600

    added edit link

--- controllers/todos.js
+++ controllers/todos.js
@@ -51,5 +51,15 @@ module.exports = {
         }catch(err){
             console.log(err)
         }
+    },
+    getEditTodo: async (req,res)=>{
+        console.log(req.user)
+        try{
+            const todoItems = await Todo.find({userId:req.user.id})
+            const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
+            res.render('edit.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+        }catch(err){
+            console.log(err)
+        }
     }
 }    
--- routes/todos.js
+++ routes/todos.js
@@ -13,4 +13,6 @@ router.put('/markIncomplete', todosController.markIncomplete)
 
 router.delete('/deleteTodo', todosController.deleteTodo)
 
+router.get('/edit', todosController.getEditTodo)
+
 module.exports = router
